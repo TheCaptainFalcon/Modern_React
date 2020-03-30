926001d7eb712f0a87eb4e4c97ad46a40fcb45de
Joseph Kim <josephkim570@gmail.com>
2020-03-30 09:05:08 -0400

    Added ternary operater to show a 'win'  when a certain number appears and hides the button as part of the arg

--- clicker_ex/src/Components/Clicker.js
+++ clicker_ex/src/Components/Clicker.js
@@ -4,22 +4,21 @@ class Clicker extends Component {
     constructor(props) {
         super(props);
         this.state = { 
-            clicked: false,
             number: 1
         }
     };
         
     buttonClick() {
         let randomNumber = Math.floor(Math.random() * 10) + 1;
-        this.setState({number: randomNumber})
+        this.setState({ number: randomNumber });
     }
     
     render() { 
 
         return (
             <div>
-                <p>Number is {this.state.number} </p>
-                <button onClick={this.buttonClick.bind(this)}>Click me</button>
+                <h3> Number is {this.state.number} </h3>
+                <h2> {this.state.number === 7? 'You Win!' : <button onClick={this.buttonClick.bind(this)}>Click me</button>} </h2>
             </div>
           );
     }
