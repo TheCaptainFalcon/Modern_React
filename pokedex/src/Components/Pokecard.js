6b4ef602762417ecab520082c4595733114df9e8
Joseph Kim <josephkim570@gmail.com>
2020-03-30 04:37:44 -0400

    Added new img src per pokecards - utilizes new function that has ternary operator to format numbers (000) based on url params

--- pokedex/src/Components/Pokecard.js
+++ pokedex/src/Components/Pokecard.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import './Pokecard.css';
 
-const pokemon = 'https://raw.githubusercontent.com/pokeapi/sprites/master/sprites/pokemon/';
+// Starter pixel image source
+// const pokemon = 'https://raw.githubusercontent.com/pokeapi/sprites/master/sprites/pokemon/';
+
+// New image source (must incorporate '001' format)
+const pokemon = 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/';
+
+// Ternary operator to format numbers to "000" to match pokedex img params
+let numberFormat = (number) => (number <= 152 ? `00${number}`.slice(-3): number);
 
 
 class Pokecard extends Component {
     render() {
-        let pokemonImage = `${pokemon}${this.props.id}.png`
+        let pokemonImage = `${pokemon}${numberFormat(this.props.id)}.png`
         return(
             <div className="Pokecard">
                 <h1>{this.props.name}</h1>
